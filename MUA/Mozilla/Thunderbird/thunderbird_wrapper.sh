09a4c8377200e3f2c74f8f7b4075dbf0170cc5f1
Sascha Rommelfangen <sascha@rommelfangen.de>
2017-05-24 15:49:49 +0200

    python3

--- MUA/Mozilla/Thunderbird/thunderbird_wrapper.sh
+++ MUA/Mozilla/Thunderbird/thunderbird_wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 MESSAGE=`cat $1`
 SUBJECT=`head -n 1 $1`
-/opt/local/bin/python2.7 /Users/rommelfs/Scripts/mail_to_misp/mail_to_misp.py "$MESSAGE" "$SUBJECT"
+/Library/Frameworks/Python.framework/Versions/3.4/bin/python3 /Users/rommelfs/Scripts/mail_to_misp/mail_to_misp.py "$MESSAGE" "$SUBJECT"
 
